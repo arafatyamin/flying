339d741c5fee2e99ef2000d9e7c32f47977f29d8
arafatyamin <mdh631987@gmail.com>
2021-01-31 16:44:10 +0600

    complete

--- custom.js
+++ custom.js
@@ -33,5 +33,16 @@ submitBtn.addEventListener('click', function () {
      const bookingArea = document.getElementById('booking-area')
      bookingArea.style.display = "none";
      const confirmArea = document.getElementById('confirm-area')
-     confirmArea.style.display="block"
+     confirmArea.style.display = "block"
+     // clear cache
+     document.getElementById('economy-count').value = "0";
+     document.getElementById('firstClass-count').value = "0";
+})
+// return home page
+const doneBtn = document.getElementById('done');
+doneBtn.addEventListener('click', function () {
+     const confirmArea = document.getElementById('confirm-area')
+     confirmArea.style.display = "none"
+     const bookingArea = document.getElementById('booking-area')
+     bookingArea.style.display = "block";
 })
